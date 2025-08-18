8fd1057b9265dd9f341f44bf4ce2a9b377e9c1fd
Carlos Gonzalez <cgonzalez@litoprocess.com>
2025-08-18 10:00:02 -0600

    ajuste de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JornadasController;
+use App\Http\Controllers\PrdController;
+use App\Http\Controllers\SolicitudController;
+use App\Http\Controllers\SolicitudesRelacionController;
+use App\Livewire\Solicitud\Indicadores;
+use App\Livewire\Solicitud\RelacionHoras;
+use App\Livewire\Solicitud\RelacionJornadas;
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
 
@@ -17,6 +24,7 @@ use Livewire\Livewire;
 
 Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::middleware(['authcustom'])->get('/logout', [HomeController::class, 'logout'])->name('logout');
+Route::middleware(['authcustom'])->get('/dashboard', [HomeController::class, 'main'])->name('main');
 
 Route::get('/litoapps', function () {
     return redirect('https://servicios.litoprocess.com/litoapps/index.php');
