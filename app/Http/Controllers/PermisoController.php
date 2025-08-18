21839b305c5370295d35d58e893a7ebfc933ef0d
Carlos Gonzalez <cgonzalez@litoprocess.com>
2025-08-18 10:02:54 -0600

    creacion de controllador de permisos

---
+++ app/Http/Controllers/PermisoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PermisoController extends Controller
+{
+    /**
+     * Obtiene los permisos del usuario autenticado.
+     *
+     * @return array
+     */
+    public function getPermisos()
+    {
+        return collect();
+    }
+}
